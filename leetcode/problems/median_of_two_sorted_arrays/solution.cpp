119ffe9cc0794e0f6c1a9b99300b451217ced913
Bang Nguyen Huu <hucancode@gmail.com>
2022-10-07 06:09:56 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (cpp)

--- leetcode/problems/median_of_two_sorted_arrays/solution.cpp
+++ leetcode/problems/median_of_two_sorted_arrays/solution.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        nums1.reserve(nums1.size() + nums2.size());
-        for(const auto& x: nums2) {
-            nums1.insert(upper_bound(nums1.begin(), nums1.end(), x), x);
-        }
-        int n = nums1.size();
+        int n = nums1.size() + nums2.size();
         if(n == 0) {
             return 0;
         }
-        if(n == 1) {
-            return nums1[0];
+        nums1.reserve(n);
+        for(const auto& x: nums2) {
+            nums1.insert(upper_bound(nums1.begin(), nums1.end(), x), x);
         }
         if(n%2 == 1) {
             return nums1[n/2];
