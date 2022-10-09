121eabf6154c60fcc810426af1fa626fa2a1ba62
Bang Nguyen Huu <hucancode@gmail.com>
2022-10-09 05:23:22 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (cpp)

--- leetcode/problems/longest_increasing_subsequence/solution.cpp
+++ leetcode/problems/longest_increasing_subsequence/solution.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& arr) {
-        // O(n^2) solution
-        // f[i] = length of the LIS that has tail arr[i]
-        vector<int> f;
-        f.resize(arr.size());
-        f[0] = 1;
+        // O(nlogn) solution
+        // tail[i] = smallest tail for LIS length i
+        vector<int> tail;
+        tail.reserve(arr.size());
+        tail.push_back(arr[0]);
         for(int i=1;i<arr.size();i++) {
-            f[i] = 1;
-            for(int j=0;j<i;j++) {
-              if(arr[j] < arr[i]) {
-                f[i] = max(f[i], f[j] + 1);
-              }
+            auto it = lower_bound(tail.begin(), tail.end(), arr[i]);
+            if(it == tail.end()) {
+              // if this new arr[i] bigger than every LIS tail ever found, we have new LIS
+              tail.push_back(arr[i]);
+            } else {
+              // if this new arr[i] fit in existing LIS, update LIS
+              *it = arr[i];
             }
         }
-        return *max_element(f.begin(), f.end());
+        return tail.size();
     }
 };
