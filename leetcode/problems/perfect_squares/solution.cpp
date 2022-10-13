adc177a1f9fa8bce43e7dea189e805a179d5cee2
Bang Nguyen Huu <hucancode@gmail.com>
2022-10-13 01:43:53 +0000

    Sync LeetCode submission - Perfect Squares (cpp)

---
+++ leetcode/problems/perfect_squares/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> f(n+1, 10000);
+        f[0] = 0;
+        for(int i = 1;i<=n;i++) {
+            int k = 1;
+            int j = i-k*k;
+            while(j >= 0) {
+                f[i] = min(f[i], f[j] + 1);
+                k++;
+                j = i - k*k;
+            }
+        }
+        return f[n];
+    }
+};
