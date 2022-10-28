24e7c6fa539a3a482b621ecd786bdefe0c8dd0da
Bang Nguyen Huu <hucancode@gmail.com>
2022-10-28 10:57:53 +0000

    Sync LeetCode submission - Minimum One Bit Operations to Make Integers Zero (cpp)

---
+++ leetcode/problems/minimum_one_bit_operations_to_make_integers_zero/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int removeFirstSetBit(int n) {
+        return n&(n-1);
+    }
+    int getFirstSetBit(int n) {
+        return n - removeFirstSetBit(n);
+    }
+    int costToClear(int bit) {
+        return (1<<(bit+1)) - 1;
+    }
+    int minimumOneBitOperations(int n) {
+        vector<int> f(31);// f[i] = cost to make n(xxxx) to be 0000
+        vector<int> g(31);// g[i] = cost to make n(xxxx) to be 1000
+        f[0] = (n&1)?1:0;
+        g[0] = (n&1)?0:1;
+        for(int i = 1;i<31;i++) {
+            bool isSet = (n&(1<<i)) != 0;
+            if(isSet) {
+                g[i] = f[i-1];
+                f[i] = g[i-1]+1;// cost to make 1xxxx -> 11000 -> 1000
+                f[i] += costToClear(i-1);// cost to make 1000 to become 0000
+            } else {
+                f[i] = f[i-1];
+                g[i] = g[i-1]+1;// cost to make 0xxxx -> 01000 -> 11000
+                g[i] += costToClear(i-1);// cost to make 1000 to become 0000
+            }
+        }
+        return f[30];
+    }
+};
