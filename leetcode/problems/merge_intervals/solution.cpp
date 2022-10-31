14976c8befe83b4950825c5ad666310ead23f367
Bang Nguyen Huu <hucancode@gmail.com>
2022-10-31 10:37:29 +0000

    Sync LeetCode submission - Merge Intervals (cpp)

--- leetcode/problems/merge_intervals/solution.cpp
+++ leetcode/problems/merge_intervals/solution.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
+    bool overlap(int l1, int r1, int l2, int r2) {
+        int l = max(l1, l2);
+        int r = min(r1, r2);
+        return l<=r;
+    }
+    void merge(vector<int>& a, vector<int>& b) {
+        a[0] = min(a[0],b[0]);
+        a[1] = max(a[1],b[1]);
+    }
     vector<vector<int>> merge(vector<vector<int>>& a) {
         sort(a.begin(), a.end(), 
-            [](const vector<int>& a, const vector<int>& b) -> bool
-        { 
-            return a[0] < b[0]; 
-        });
-        int l = 0;
-        int r = 1;
-        while(l < a.size() - 1) {
-            r = l;
-            while(r < a.size() - 1) {
-                if(a[r+1][0] > a[l][1]) {
-                    break;
-                }
-                r++;
-                a[l][1] = max(a[l][1], a[r][1]);
+            [](const vector<int>& a, const vector<int>& b) { 
+                return a[0] < b[0];
+            });
+        vector<vector<int>> ret;
+        ret.reserve(a.size());
+        auto l = a.begin();
+        auto r = l+1;
+        while(r!= a.end()) {
+            if(overlap((*l)[0], (*l)[1], (*r)[0], (*r)[1])) {
+                merge(*l, *r);
+            } else {
+                r = a.erase(l+1,r);
+                l = r;
             }
-            if(r > l) {
-                a.erase(a.begin()+l+1, a.begin()+r+1);
-            }
-            l++;
+            r++;
         }
+        a.erase(l+1,r);
         return a;
     }
 };
