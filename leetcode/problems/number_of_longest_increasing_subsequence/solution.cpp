c12d6d8fffbf7900e17f3d86a07a0db502148689
Bang Nguyen Huu <hucancode@gmail.com>
2022-12-05 07:29:00 +0000

    Sync LeetCode submission - Number of Longest Increasing Subsequence (cpp)

---
+++ leetcode/problems/number_of_longest_increasing_subsequence/solution.cpp
@@ -0,0 +1,57 @@
+typedef pair<int, int> pi;
+class FenwickTree {
+    vector<pi> data;
+public:
+    FenwickTree(vector<int>& nums) {
+        
+    }
+    int goUp(int i) {
+        return i | (i+1);
+    }
+    int goLeft(int i) {
+        return i & (i+1) - 1;
+    }
+    pi query(int i) {
+        return pi(0,0);
+    }
+    void update(int i, int val) {
+        
+    }
+};
+
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<pi> f(n);
+        f[0] = {1,1};
+        for(int i = 1;i<n;i++) {
+            int len = 1;
+            int count = 1;
+            for(int j=0;j<i;j++) {
+                if(nums[j] < nums[i]) {
+                    int nextLen = f[j].first + 1;
+                    int nextCount = f[j].second;
+                    if(nextLen > len) {
+                        len = nextLen;
+                        count = nextCount;
+                    } else if(nextLen == len) {
+                        count += nextCount;
+                    }
+                }
+            }
+            f[i] = {len, count};
+        }
+        int bestLen = 0;
+        int ret = 0;
+        for(int i = 0;i<n;i++) {
+            if(f[i].first > bestLen) {
+                bestLen = f[i].first;
+                ret = f[i].second;
+            } else if(f[i].first == bestLen) {
+                ret += f[i].second;
+            }
+        }
+        return ret;
+    }
+};
