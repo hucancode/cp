29dd37175e295e318b81e5401e0b89d06f0ac05d
Bang Nguyen Huu <hucancode@gmail.com>
2022-12-15 03:22:37 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock IV (cpp)

--- leetcode/problems/best_time_to_buy_and_sell_stock_iv/solution.cpp
+++ leetcode/problems/best_time_to_buy_and_sell_stock_iv/solution.cpp
@@ -1,36 +1,46 @@
 class Solution {
 public:
     int maxProfit(int maxk, vector<int>& prices) {
+        int ret = 0;
         int n = prices.size();
         if(n < 2) {
             return 0;
         }
         vector<vector<int>> f(maxk+1, vector<int>(n, 0));
-        vector<vector<int>> delta(n, vector<int>(n, 0));
-        int ret = 0;
-        for(int i = 1;i<n;i++) {
+        vector<vector<int>> g(maxk+1, vector<int>(n, 0));
+        // f[i][j] = max profit if buy at j, have bought i times
+        // g[i][j] = max profit if sell at j, have sold i times
+        for(int i = 1;i<=maxk;i++) {
             for(int j = 0;j<i;j++) {
-                delta[i][j] = prices[i] - prices[j];
+                g[i][j] = -2e9;
+                f[i][j] = (j>0?f[i][j-1]:0) - prices[j];
             }
         }
-        for(int i = 1;i<n;i++) {
-            for(int k = 1;k<=maxk;k++) {
-                f[k][i] = max(f[k][i], f[k-1][i]);
-                f[k][i] = max(f[k][i], f[k][i-1]);
-                //cout<<"max profit till now = "<<f[k][i]<<endl;
-                for(int j = 0;j<i;j++) {
-                    if(delta[i][j] < 0) {
-                        continue;
-                    }
-                    //cout<<"check range a["<<j<<"]="<<prices[j]<<", a["<<i<<"]="<<prices[i]<<", delta = "<<delta<<endl;
-                    if(j > 0) {
-                        f[k][i] = max(f[k][i], f[k-1][j-1] + delta[i][j]);
-                    } else {
-                        f[k][i] = max(f[k][i], delta[i][j]);
-                    }
-                }
+        for(int i = 1;i<=maxk;i++) {
+            for(int j = 1;j<n;j++) {
+                int buy = g[i-1][j-1]-prices[j];
+                int skip = f[i][j-1];
+                f[i][j] = max(buy, skip);
             }
+            for(int j = 1;j<n;j++) {
+                int sell = f[i][j-1]+prices[j];
+                int skip = g[i][j-1];
+                g[i][j] = max(sell, skip);
+                ret = max(ret, g[i][j]);
+            }
+            continue;
+            cout<<"round "<<i<<endl;
+            cout<<", f = ";
+            for(auto x: f[i]) {
+                cout<<x<<" ";
+            }
+            cout<<endl;
+            cout<<", g = ";
+            for(auto x: g[i]) {
+                cout<<x<<" ";
+            }
+            cout<<endl;
         }
-        return f[maxk][n-1];
+        return ret;
     }
 };
