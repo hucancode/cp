7ad0ba630d9733b5f415492776d46c4693f78a79
Bang Nguyen Huu <hucancode@gmail.com>
2022-12-17 02:07:42 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation (cpp)

--- leetcode/problems/evaluate_reverse_polish_notation/solution.cpp
+++ leetcode/problems/evaluate_reverse_polish_notation/solution.cpp
@@ -1,34 +1,35 @@
+typedef long long ll;
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<int> s;
+        stack<ll> s;
         for(int i=0;i<tokens.size();i++) {
             auto token = tokens[i];
             if(token == "+") {
-                int a = s.top();
+                auto a = s.top();
                 s.pop();
-                int b = s.top();
+                auto b = s.top();
                 s.pop();
                 //cout<<"push "<<a<<"+"<<b<<endl;
                 s.push(a+b);
             } else if(token == "-") {
-                int a = s.top();
+                auto a = s.top();
                 s.pop();
-                int b = s.top();
+                auto b = s.top();
                 s.pop();
                 s.push(b-a);
                 //cout<<"push "<<a<<"-"<<b<<endl;
             } else if(token == "*") {
-                int a = s.top();
+                auto a = s.top();
                 s.pop();
-                int b = s.top();
+                auto b = s.top();
                 s.pop();
                 s.push(a*b);
                 //cout<<"push "<<a<<"*"<<b<<endl;
             } else if(token == "/") {
-                int a = s.top();
+                auto a = s.top();
                 s.pop();
-                int b = s.top();
+                auto b = s.top();
                 s.pop();
                 s.push(b/a);
                 //cout<<"push "<<a<<"/"<<b<<endl;
