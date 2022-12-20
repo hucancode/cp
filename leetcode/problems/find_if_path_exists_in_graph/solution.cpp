a18a28556c57266298e3412b12e55bb1813413aa
Bang Nguyen Huu <hucancode@gmail.com>
2022-12-20 01:28:52 +0000

    Sync LeetCode submission - Find if Path Exists in Graph (cpp)

--- leetcode/problems/find_if_path_exists_in_graph/solution.cpp
+++ leetcode/problems/find_if_path_exists_in_graph/solution.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
+    vector<int> parent;
+    
+    int find(int x) {
+        if(parent[x] == x) {
+            return x;
+        }
+        return find(parent[x]);
+    }
+    void unionXY(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        parent[py] = px;
+    }
+
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        map<int, vector<int>> adj;
+        parent.resize(n);
+        iota(parent.begin(), parent.end(), 0);
         for(auto e: edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
+            unionXY(e[0], e[1]);
         }
-        vector<bool> vis(n, false);
-        stack<int> st;
-        st.push(source);
-        while(!st.empty()) {
-            auto u = st.top();
-            st.pop();
-            if(vis[u]) {
-                continue;
-            }
-            vis[u] = true;
-            if(u == destination) {
-                return true;
-            }
-            for(auto v: adj[u]) {
-                st.push(v);
-            }
-        }
-        return false;
+        return find(source) == find(destination);
     }
 };
