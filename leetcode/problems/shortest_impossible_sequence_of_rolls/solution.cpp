c29186ae7968a0d6c6ab8bd2ba8e4cb62e221b5f
Bang Nguyen Huu <hucancode@gmail.com>
2023-04-01 05:59:42 +0000

    Sync LeetCode submission - Shortest Impossible Sequence of Rolls (cpp)

--- leetcode/problems/shortest_impossible_sequence_of_rolls/solution.cpp
+++ leetcode/problems/shortest_impossible_sequence_of_rolls/solution.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
     int shortestSequence(vector<int>& rolls, int k) {
         vector<bool> vis(k+1, false);
-        int count = 0;
-        int ret = 1;
+        int count = 0, ret = 1;
         for(auto x: rolls) {
-            if(!vis[x]) {
-                count++;
-                vis[x] = true;
-                if(count == k) {
-                    fill(vis.begin(), vis.end(), false);
-                    count = 0;
-                    ret++;
-                }
-            }
+            if(vis[x]) continue;
+            vis[x] = true;
+            if(++count != k) continue;
+            fill(vis.begin(), vis.end(), false);
+            count = 0;
+            ret++;
         }
         return ret;
     }
