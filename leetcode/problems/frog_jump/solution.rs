d0d098c754e6406e3b270dc1b53a25a44dc2ac1d
Bang Nguyen Huu <hucancode@gmail.com>
2023-05-12 02:22:47 +0000

    Sync LeetCode submission - Frog Jump (rust)

--- leetcode/problems/frog_jump/solution.rs
+++ leetcode/problems/frog_jump/solution.rs
@@ -4,25 +4,24 @@ impl Solution {
         let mut f = vec![vec![];n];
         f[0].push(0);
         for i in 0..n {
-            for k in 0..f[i].len() {
+            let m = f[i].len();
+            for k in 0..m {
                 for d in [1, 0, -1].iter() {
                     let jump = f[i][k] + d;
                     if jump <= 0 {
                         continue;
                     }
                     let target = stones[i]+jump;
-                    match stones.binary_search(&target) {
-                        Ok(j) => {
-                            match f[j].binary_search(&jump) {
-                                Err(i) => f[j].insert(i, jump),
-                                _ => {},
-                            };
-                        },
-                        _ => {},
+                    if let Ok(j) = stones.binary_search(&target) {
+                        if let Err(i) = f[j].binary_search(&jump) {
+                            f[j].insert(i, jump);
+                        }
                     }
                 }
             }
         }
-        return !f.last().unwrap().is_empty();
+        return !f.last()
+            .unwrap_or(&Vec::new())
+            .is_empty();
     }
 }
