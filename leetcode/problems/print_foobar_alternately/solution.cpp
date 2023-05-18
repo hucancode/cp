a11774e2a134fa6d0f144cc7ebf6224776ec41b9
Bang Nguyen Huu <hucancode@gmail.com>
2023-05-18 01:52:12 +0000

    Sync LeetCode submission - Print FooBar Alternately (cpp)

--- leetcode/problems/print_foobar_alternately/solution.cpp
+++ leetcode/problems/print_foobar_alternately/solution.cpp
@@ -1,32 +1,31 @@
-#include <mutex>
 class FooBar {
 private:
     int n;
-    mutex mfoo;
-    mutex mbar;
+    mutex fooMtx;
+    mutex barMtx;
+
 public:
     FooBar(int n) {
         this->n = n;
-        mbar.lock();
+        barMtx.lock();
     }
 
     void foo(function<void()> printFoo) {
-        
         for (int i = 0; i < n; i++) {
-            mfoo.lock();
+            fooMtx.lock();
         	// printFoo() outputs "foo". Do not change or remove this line.
         	printFoo();
-            mbar.unlock();
+            barMtx.unlock();
         }
     }
 
     void bar(function<void()> printBar) {
         
         for (int i = 0; i < n; i++) {
-            mbar.lock();
+            barMtx.lock();
         	// printBar() outputs "bar". Do not change or remove this line.
         	printBar();
-            mfoo.unlock();
+            fooMtx.unlock();
         }
     }
 };
