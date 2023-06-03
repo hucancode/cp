85f39a48ff3a359c3669aa16b870543c6c959477
Bang Nguyen Huu <hucancode@gmail.com>
2023-06-03 14:48:51 +0000

    Sync LeetCode submission - Remove All Adjacent Duplicates In String (cpp)

--- leetcode/problems/remove_all_adjacent_duplicates_in_string/solution.cpp
+++ leetcode/problems/remove_all_adjacent_duplicates_in_string/solution.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        bool removed = false;
-        for(int i = s.size()-1;i>0;i--) {
-            if(s[i] == s[i-1]) {
-                s.erase(s.begin()+i-1, s.begin()+i+1);
+        string ret;
+        for(char c: s) {
+            if(!ret.empty() && *ret.rbegin() == c) {
+                ret.pop_back();
+                continue;
             }
+            ret.push_back(c);
         }
-        return s;
+        return ret;
     }
 };
