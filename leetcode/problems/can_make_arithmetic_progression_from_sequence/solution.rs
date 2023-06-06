8c6c45c0f0fbba5815517483b674cf2839db8f6c
Bang Nguyen Huu <hucancode@gmail.com>
2023-06-06 00:54:51 +0000

    Sync LeetCode submission - Can Make Arithmetic Progression From Sequence (rust)

---
+++ leetcode/problems/can_make_arithmetic_progression_from_sequence/solution.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn can_make_arithmetic_progression(arr: Vec<i32>) -> bool {
+        let n = arr.len() as i32;
+        let min = *arr.iter().min().unwrap_or(&0);
+        let max = *arr.iter().max().unwrap_or(&0);
+        if (max-min)%(n-1) != 0 {
+            return false;
+        }
+        let d = (max-min)/(n-1);
+        if d == 0 {
+            return true;
+        }
+        let mut vis = vec![false;n as usize];
+        arr.iter()
+            .map(|x| ((x-min)/d) as usize)
+            .for_each(|x| vis[x] = true);
+        vis.iter().all(|&x| x)
+    }
+}
