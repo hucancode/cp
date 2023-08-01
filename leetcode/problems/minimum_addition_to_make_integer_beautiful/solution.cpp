698a0b3a463f7445ada7bffe189b392356d98d45
Bang Nguyen Huu <hucancode@gmail.com>
2023-08-01 08:37:08 +0000

    Sync LeetCode submission - Minimum Addition to Make Integer Beautiful (cpp)

--- leetcode/problems/minimum_addition_to_make_integer_beautiful/solution.cpp
+++ leetcode/problems/minimum_addition_to_make_integer_beautiful/solution.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    int sumDigit(long long n) {
-        int ret = 0;
-        long long k = 1e12;
-        while(n!= 0) {
-            ret += n/k;
-            n = n%k;
-            k/=10;
-        }
-        return ret;
-    }
     long long makeIntegerBeautiful(long long n, int target) {
-        int sum = sumDigit(n);
-        if(sum <= target) {
-            return 0;
+        int sum = 0;
+        auto m = n;
+        while(m > 0) {
+            sum += m%10;
+            m/=10;
         }
+        m = n;
+        int remainder = 0;
         long long ret = 0;
-        long long k = 1;
-        int remaining = 0;
-        //cout<<"initial sum = "<<sum<<endl;
-        while(sum+remaining > target) {
-            int d = n%10;
-            sum -= d;
-            int a = max(0,10-d-remaining);
-            //cout<<"d "<<d<<" a "<<a<<" r "<<remaining<<endl;
-            remaining = 1;
-            ret += a*k;
-            n /= 10;
-            k *= 10;
+        long long base = 1;
+        //cout<<"__________"<<endl;
+        while(sum > target || remainder != 0) {
+            int d = m%10;
+            //cout<<"check "<<d<<" sum = "<<sum<<endl;
+            if(d + remainder == 10) {
+                sum -= d;
+            } else {
+                sum += remainder;
+                d += remainder;
+                if(sum <= target) {
+                    break;
+                }
+                sum -= d;
+                ret += base * (10 - d);
+                remainder = 1;
+            }
+            m /= 10;
+            base *= 10;
         }
         return ret;
     }
