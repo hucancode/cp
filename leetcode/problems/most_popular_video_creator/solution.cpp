252b1da93d83f11f076d92f3087a87202278dfac
Bang Nguyen Huu <hucancode@gmail.com>
2023-08-01 08:02:17 +0000

    Sync LeetCode submission - Most Popular Video Creator (cpp)

--- leetcode/problems/most_popular_video_creator/solution.cpp
+++ leetcode/problems/most_popular_video_creator/solution.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
     vector<vector<string>> mostPopularCreator(vector<string>& creators, vector<string>& ids, vector<int>& views) {
-        vector<vector<string>> ret;
-        map<string, long long> totalView;
-        map<string, int> topVideo;
         int n = creators.size();
+        map<string, long long> creator_view;
+        map<string, pair<int, string>> creator_top;
         for(int i = 0;i<n;i++) {
-            auto& c = creators[i];
-            auto& id = ids[i];
-            auto& view = views[i];
-            totalView[c] += view;
-            if(topVideo.find(c) == topVideo.end()) {
-                topVideo[c] = i;
-            }
-            auto topId = topVideo[c];
-            if(views[topId] < view || (views[topId] == view && ids[topId] > id)) {
-                topVideo[c] = i;
+            auto c = creators[i];
+            auto id = ids[i];
+            auto v = views[i];
+            creator_view[c] += v;
+            if(creator_top.find(c) == creator_top.end()) {
+                creator_top[c] = make_pair(v, id);
+            } else {
+                int top_v;
+                string top;
+                tie(top_v, top) = creator_top[c];
+                if(v > top_v || (top_v == v && id.compare(top) < 0)) {
+                    creator_top[c] = make_pair(v, id);
+                }
             }
         }
-        vector<pair<long long, string>> f;
-        for(auto& item: totalView) {
-            f.emplace_back(item.second, item.first);
-        }
-        sort(f.rbegin(), f.rend());
-        long long k = f[0].first;
-        for(auto& item: f) {
-            if(item.first != k) {
-                break;
+        vector<vector<string>> ret;
+        long long best = -1;
+        for(auto kv: creator_view) {
+            string k = kv.first;
+            long long v = (long long)kv.second;
+            if(v < best) {
+                continue;
             }
-            auto creator = item.second;
-            auto top = ids[topVideo[creator]];
-            ret.push_back({creator, top});
+            if(v > best) {
+                ret.clear();
+            }
+            best = v;
+            ret.push_back({k, creator_top[k].second});
         }
         return ret;
     }
