27ebe1eb60131bcc03137cd0454b0ea09b2909dc
Bang Nguyen Huu <hucancode@gmail.com>
2023-08-02 00:19:02 +0000

    Sync LeetCode submission - Permutations (cpp)

--- leetcode/problems/permutations/solution.cpp
+++ leetcode/problems/permutations/solution.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        int n = nums.size();
+        sort(nums.begin(), nums.end());
         vector<vector<int>> ret;
-        if(n == 1) {
-            ret.push_back(nums);
-            return ret;
-        }
-        vector<int> b(nums.begin()+1, nums.end());
-        auto pb = permute(b);
-        auto a = nums[0];
-        for(const auto x: pb) {
-            int i = 0;
-            while(true) {
-                vector<int> c(x.begin(), x.end());
-                c.insert(c.begin()+i, a);
-                ret.push_back(c);
-                if(i == x.size()) {
-                    break;
-                }
-                i++;
-            }
-        }
+        do {
+            vector<int> p(nums.begin(), nums.end());
+            ret.push_back(p);
+        } while(next_permutation(nums.begin(), nums.end()));
         return ret;
     }
 };
