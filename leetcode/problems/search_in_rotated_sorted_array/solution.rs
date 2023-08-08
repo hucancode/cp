c3bd631ed55626750ac90f191df96d323b61f24b
Bang Nguyen Huu <hucancode@gmail.com>
2023-08-08 05:06:54 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (rust)

---
+++ leetcode/problems/search_in_rotated_sorted_array/solution.rs
@@ -0,0 +1,35 @@
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let n = nums.len();
+        let mut r = n-1;
+        let mut l = 0;
+        while l < r {
+            let m = (l+r)/2;
+            let good = nums[m] < nums[r];
+            if good {
+                r = m;
+            } else {
+                l = m+1;
+            }
+        }
+        if target > nums[n-1] {
+            r = if l == 0 {n-1} else {l-1};
+            l = 0;
+        } else {
+            r = n-1;
+        }
+        while l < r {
+            let m = (l+r)/2;
+            let good = nums[m] >= target;
+            if good {
+                r = m;
+            } else {
+                l = m+1;
+            }
+        }
+        if nums[l] != target {
+            return -1;
+        }
+        return l as i32;
+    }
+}
