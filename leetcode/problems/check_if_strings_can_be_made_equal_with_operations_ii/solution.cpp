b0a430cd88598a56839a2f3602b0171dbdf4e30c
Bang Nguyen Huu <hucancode@gmail.com>
2023-09-02 14:39:31 +0000

    Sync LeetCode submission - Check if Strings Can be Made Equal With Operations II (cpp)

---
+++ leetcode/problems/check_if_strings_can_be_made_equal_with_operations_ii/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        vector<char> ac1;
+        vector<char> ac2;
+        vector<char> bd1;
+        vector<char> bd2;
+        int n = s1.size();
+        for(int i = 0;i<n;i+=2) {
+            ac1.push_back(s1[i]);
+            ac2.push_back(s2[i]);
+        }
+        for(int i = 1;i<n;i+=2) {
+            bd1.push_back(s1[i]);
+            bd2.push_back(s2[i]);
+        }
+        sort(ac1.begin(),ac1.end());
+        sort(ac2.begin(),ac2.end());
+        sort(bd1.begin(),bd1.end());
+        sort(bd2.begin(),bd2.end());
+        return ac1 == ac2 && bd1 == bd2;
+    }
+};
