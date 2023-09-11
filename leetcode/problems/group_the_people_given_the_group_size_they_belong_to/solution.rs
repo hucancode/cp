f8c58b94996cb7bc2fd1dc32421e89fa6a703fe6
Bang Nguyen Huu <hucancode@gmail.com>
2023-09-11 00:23:35 +0000

    Sync LeetCode submission - Group the People Given the Group Size They Belong To (rust)

--- leetcode/problems/group_the_people_given_the_group_size_they_belong_to/solution.rs
+++ leetcode/problems/group_the_people_given_the_group_size_they_belong_to/solution.rs
@@ -2,25 +2,16 @@ use std::collections::HashMap;
 impl Solution {
     pub fn group_the_people(group_sizes: Vec<i32>) -> Vec<Vec<i32>> {
         let mut map: HashMap<usize, Vec<i32>> = HashMap::new();
-        let mut ret = vec![];
-        for (i, n) in group_sizes.iter().enumerate() {
-            map.entry(*n as usize)
+        for (i, &n) in group_sizes.iter().enumerate() {
+            map.entry(n as usize)
                 .or_default()
                 .push(i as i32);
         }
-        for (n, arr) in map.iter() {
-            let mut i = 0;
-            while i < arr.len() {
-                let group = arr
-                    .clone()
-                    .into_iter()
-                    .skip(i)
-                    .take(*n)
-                    .collect();
-                ret.push(group);
-                i += n;
-            }
-        }
-        return ret;
+        map.into_iter()
+            .map(|(n, arr)| arr.chunks(n)
+                .map(|arr| arr.to_vec())
+                .collect::<Vec<Vec<i32>>>())
+            .flatten()
+            .collect()
     }
 }
