e95c19c7643f5cce58215ab4a1716e08d6640289
Bang Nguyen Huu <hucancode@gmail.com>
2023-09-13 00:49:24 +0000

    Sync LeetCode submission - Candy (rust)

--- leetcode/problems/candy/solution.rs
+++ leetcode/problems/candy/solution.rs
@@ -4,12 +4,11 @@ impl Solution {
     pub fn candy(ratings: Vec<i32>) -> i32 {
         let n = ratings.len();
         let mut candy = vec![1;n];
-        for i in (1..n).filter(|&i| ratings[i] > ratings[i-1]) {
-            candy[i] = max(candy[i], candy[i-1] + 1);
-        }
-        for i in (0..n-1).rev().filter(|&i| ratings[i] > ratings[i+1]) {
-            candy[i] = max(candy[i], candy[i+1] + 1);
-        }
+        let it1 = (1..n).map(|i| (i, i-1));
+        let it2 = (0..n-1).rev().map(|i| (i, i+1));
+        it1.chain(it2)
+            .filter(|(i,j)| ratings[*i] > ratings[*j])
+            .for_each(|(i,j)| candy[i] = max(candy[i], candy[j] + 1));
         candy.iter().sum()
     }
 }
