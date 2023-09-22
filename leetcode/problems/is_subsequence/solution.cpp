16539dabda19ade9d16767ce10433b60a5e3a597
Bang Nguyen Huu <hucancode@gmail.com>
2023-09-22 01:02:26 +0000

    Sync LeetCode submission - Is Subsequence (cpp)

--- leetcode/problems/is_subsequence/solution.cpp
+++ leetcode/problems/is_subsequence/solution.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int i = 0;
-        int j = 0;
-        int n = s.size();
-        int m = t.size();
-        if(n == 0) {
-            return true;
+        int n = t.size(), m = s.size();
+        int i = 0, j = 0;
+        while(j < m && i < n) {
+            if(t[i] == s[j]) j++;
+            i++;
         }
-        while(j < m) {
-            if(s[i] == t[j]) {
-                i++;
-            }
-            j++;
-            if(i == n) {
-                return true;
-            }
-        }
-        return false;
+        return j >= m;
     }
 };
