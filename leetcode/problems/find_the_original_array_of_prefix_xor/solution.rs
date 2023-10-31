383d4a9c065f4c09bfc2b5af840a01ef7d91fba8
Bang Nguyen Huu <hucancode@gmail.com>
2023-10-31 07:04:51 +0000

    Sync LeetCode submission - Find The Original Array of Prefix Xor (rust)

--- leetcode/problems/find_the_original_array_of_prefix_xor/solution.rs
+++ leetcode/problems/find_the_original_array_of_prefix_xor/solution.rs
@@ -1,7 +1,7 @@
 impl Solution {
     pub fn find_array(mut pref: Vec<i32>) -> Vec<i32> {
       for i in (1..pref.len()).rev() {
-        pref[i] = pref[i] ^ pref[i-1];
+        pref[i] ^= pref[i-1];
       }
       return pref;
     }
