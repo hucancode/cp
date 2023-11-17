2f10b943a880558689f1c7e38bd3dac7d66de458
Bang Nguyen Huu <hucancode@gmail.com>
2023-11-17 07:41:07 +0000

    Sync LeetCode submission - Minimize Maximum Pair Sum in Array (rust)

--- leetcode/problems/minimize_maximum_pair_sum_in_array/solution.rs
+++ leetcode/problems/minimize_maximum_pair_sum_in_array/solution.rs
@@ -2,6 +2,9 @@ impl Solution {
     pub fn min_pair_sum(mut nums: Vec<i32>) -> i32 {
         nums.sort();
         let n = nums.len();
-        (0..n/2).map(|i| nums[i] + nums[n-i-1]).max().unwrap_or(0)
+        (0..n/2)
+            .map(|i| nums[i]+nums[n-i-1])
+            .max()
+            .unwrap_or(0)
     }
 }
