1143c73e33502b447e931b62183b40f27a813c52
Bang Nguyen Huu <hucancode@gmail.com>
2023-12-15 01:33:32 +0000

    Sync LeetCode submission - Maximum Level Sum of a Binary Tree (rust)

--- leetcode/problems/maximum_level_sum_of_a_binary_tree/solution.rs
+++ leetcode/problems/maximum_level_sum_of_a_binary_tree/solution.rs
@@ -21,33 +21,35 @@ use std::cell::RefCell;
 use std::collections::VecDeque;
 impl Solution {
     pub fn max_level_sum(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        let mut ret = -1;
-        if let Some(root) = root {
-            let mut q = VecDeque::new();
-            q.push_back(root);
-            let mut max = i32::MIN;
-            let mut level = 0;
-            while !q.is_empty() {
-                let n = q.len();
-                level += 1;
-                let mut score = 0;
-                for _ in 0..n {
-                    if let Some(node) = q.pop_front() {
-                        score += node.borrow().val;
-                        if let Some(node) = node.borrow().left.clone() {
-                            q.push_back(node);
-                        }
-                        if let Some(node) = node.borrow().right.clone() {
-                            q.push_back(node);
-                        }
-                    }
+        if root.is_none() {
+            return 0;
+        }
+        let root = root.unwrap();
+        let mut q = VecDeque::new();
+        q.push_back(root);
+        let mut max_sum = i32::MIN;
+        let mut max_level = 1;
+        let mut level = 0;
+        while !q.is_empty() {
+            level += 1;
+            let mut sum = 0;
+            let mut next = VecDeque::new();
+            while let Some(node) = q.pop_front() {
+                let mut node = node.borrow_mut();
+                sum += node.val;
+                if let Some(node) = node.left.take() {
+                    next.push_back(node);
                 }
-                if score > max {
-                    max = score;
-                    ret = level;
+                if let Some(node) = node.right.take() {
+                    next.push_back(node);
                 }
             }
+            if sum > max_sum {
+                max_sum = sum;
+                max_level = level;
+            }
+            q = next;
         }
-        ret
+        max_level
     }
 }
