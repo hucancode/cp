5f3bd165d5526b1a23e96cb4ed4b21857f292d30
Bang Nguyen Huu <hucancode@gmail.com>
2023-12-21 00:15:28 +0000

    Sync LeetCode submission - Widest Vertical Area Between Two Points Containing No Points (rust)

--- leetcode/problems/widest_vertical_area_between_two_points_containing_no_points/solution.rs
+++ leetcode/problems/widest_vertical_area_between_two_points_containing_no_points/solution.rs
@@ -1,9 +1,9 @@
 impl Solution {
     pub fn max_width_of_vertical_area(mut points: Vec<Vec<i32>>) -> i32 {
         points.sort_by(|a,b| a[0].cmp(&b[0]));
-        (1..points.len())
-            .map(|i| points[i][0] - points[i-1][0])
+        points.windows(2)
+            .map(|a| a[1][0] - a[0][0])
             .max()
-            .unwrap_or(0)
+            .unwrap_or(i32::MAX)
     }
 }
