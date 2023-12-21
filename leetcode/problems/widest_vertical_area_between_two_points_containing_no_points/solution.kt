36f739d238a41dc2e73594418c22dccfb0a4c25a
Bang Nguyen Huu <hucancode@gmail.com>
2023-12-21 04:18:57 +0000

    Sync LeetCode submission - Widest Vertical Area Between Two Points Containing No Points (kotlin)

--- leetcode/problems/widest_vertical_area_between_two_points_containing_no_points/solution.kt
+++ leetcode/problems/widest_vertical_area_between_two_points_containing_no_points/solution.kt
@@ -1,10 +1,9 @@
 class Solution {
     fun maxWidthOfVerticalArea(points: Array<IntArray>): Int {
-        points.sortBy { it[0] }
         return points
-            .asSequence()
-            .windowed(2)
-            .map { it[1][0] - it[0][0] }
+            .map { it[0] }
+            .sorted()
+            .zipWithNext { a, b -> b - a }
             .max()
     }
 }
