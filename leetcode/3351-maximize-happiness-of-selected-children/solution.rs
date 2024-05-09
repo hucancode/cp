a4848241bec80bb77fcec59a81b5c42636a0ffa2
Bang Nguyen Huu <hucancode@gmail.com>
2024-05-09 08:10:02 +0000

    Sync LeetCode submission Runtime - 40 ms (28.18%), Memory - 5.2 MB (47.69%)

--- leetcode/3351-maximize-happiness-of-selected-children/solution.rs
+++ leetcode/3351-maximize-happiness-of-selected-children/solution.rs
@@ -1,17 +1,12 @@
 impl Solution {
-    pub fn maximum_happiness_sum(mut happiness: Vec<i32>, mut k: i32) -> i64 {
+    pub fn maximum_happiness_sum(mut happiness: Vec<i32>, k: i32) -> i64 {
         happiness.sort();
-        let n = happiness.len();
-        let mut ret = 0i64;
-        let mut j = 0;
-        while let Some(x) = happiness.pop() {
-            if x <= j || k == 0 {
-                break;
-            }
-            ret += x as i64 - j as i64;
-            j += 1;
-            k -= 1;
-        }
-        ret
+        happiness.into_iter()
+            .rev()
+            .enumerate()
+            .map(|(i,x)| (x - i as i32) as i64)
+            .take(k as usize)
+            .take_while(|&x| x > 0)
+            .sum()
     }
 }
