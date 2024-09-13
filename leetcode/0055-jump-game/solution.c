cf04b78b9e201dc6d6c9159c84006fab35fc3767
Bang Nguyen Huu <hucancode@gmail.com>
2024-09-13 08:16:21 +0000

    Sync LeetCode submission Runtime - 49 ms (36.23%), Memory - 10.5 MB (39.21%)

--- leetcode/0055-jump-game/solution.c
+++ leetcode/0055-jump-game/solution.c
@@ -1,14 +1,14 @@
 int max(int a, int b) {
     return a>b?a:b;
 }
-
-bool canJump(int* nums, int n) {
-    int k = 0;
+bool canJump(int* a, int n) {
+    int max_distance = 0;
     for(int i = 0;i<n;i++) {
-        if(i > k) {
+        if(i > max_distance) {
             return false;
         }
-        k = max(k, i+nums[i]);
+        int new_oppotunity = i+a[i];
+        max_distance = max(max_distance, new_oppotunity);
     }
     return true;
 }
