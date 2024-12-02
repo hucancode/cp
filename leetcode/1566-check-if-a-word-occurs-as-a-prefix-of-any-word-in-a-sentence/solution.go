6b7b2b91bc6297bc61d2d0b72756b5d98ef79279
Bang Nguyen Huu <hucancode@gmail.com>
2024-12-02 01:54:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 3.9 MB (92.94%)

---
+++ leetcode/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/solution.go
@@ -0,0 +1,8 @@
+func isPrefixOfWord(sentence string, searchWord string) int {
+    for i, w := range strings.Fields(sentence) {
+        if len(w) >= len(searchWord) && w[:len(searchWord)] == searchWord {
+            return i+1
+        }
+    }
+    return -1
+}
