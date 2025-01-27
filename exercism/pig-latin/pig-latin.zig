c5694edaaf0fb03320678f7e80da3e248d49333c
USER <genji.nguyenhuu@ntq-solution.com.vn>
2025-01-27 10:52:47 +0900

    code golf

--- exercism/pig-latin/pig-latin.zig
+++ exercism/pig-latin/pig-latin.zig
@@ -20,7 +20,6 @@ pub fn translate(allocator: mem.Allocator, phrase: []const u8) mem.Allocator.Err
     var it = mem.splitScalar(u8, phrase, ' ');
     var ret = ArrayList(u8).init(allocator);
     while (it.next()) |word| {
-        const beginWithVowel = word.len >= 1 and isVowel(word[0]);
         const beginWithXrYt = word.len >= 2 and (mem.eql(u8, word[0..2], "xr") or
             mem.eql(u8, word[0..2], "yt"));
         const quIndex = mem.indexOf(u8, word, "qu");
@@ -28,27 +27,17 @@ pub fn translate(allocator: mem.Allocator, phrase: []const u8) mem.Allocator.Err
         const noVowelBeforeQu = quIndex != null and (yIndex == null or yIndex.? > quIndex.?) and indexOfVowel(word[0..quIndex.?]) == null;
         const noVowelBeforeY = yIndex != null and yIndex.? > 0 and (quIndex == null or quIndex.? > yIndex.?) and indexOfVowel(word[0..yIndex.?]) == null;
         const vowelIndex = indexOfVowel(word);
-
+        var n: usize = 0;
         if (noVowelBeforeQu) {
-            const n = quIndex.? + 2;
-            try ret.appendSlice(word[n..]);
-            try ret.appendSlice(word[0..n]);
-            try ret.appendSlice("ay");
+            n = quIndex.? + 2;
         } else if (noVowelBeforeY) {
-            const n = yIndex.?;
-            try ret.appendSlice(word[n..]);
-            try ret.appendSlice(word[0..n]);
-            try ret.appendSlice("ay");
-        } else if (beginWithVowel or beginWithXrYt) {
-            try ret.appendSlice(word);
-            try ret.appendSlice("ay");
-        } else if (vowelIndex != null) {
-            const n = vowelIndex.?;
-            try ret.appendSlice(word[n..]);
-            try ret.appendSlice(word[0..n]);
-            try ret.appendSlice("ay");
+            n = yIndex.?;
+        } else if (!beginWithXrYt and vowelIndex != null) {
+            n = vowelIndex.?;
         }
-        try ret.append(' ');
+        try ret.appendSlice(word[n..]);
+        try ret.appendSlice(word[0..n]);
+        try ret.appendSlice("ay ");
     }
     _ = ret.pop();
     return try ret.toOwnedSlice();
--- exercism/pig-latin/pig_latin.go
+++ exercism/pig-latin/pig_latin.go
@@ -16,30 +16,22 @@ func indexOfVowel(word string) int {
 
 func Sentence(sentence string) string {
 	ret := ""
-	words := strings.Split(sentence, " ")
-	for _, word := range words {
-		beginWithVowel := len(word) >= 1 && isVowel(word[0])
+	for _, word := range strings.Split(sentence, " ") {
 		beginWithXrYt := len(word) >= 2 && (word[:2] == "xr" || word[:2] == "yt")
 		indexOfQu := strings.Index(word, "qu")
 		indexOfY := strings.Index(word, "y")
 		vowelIndex := indexOfVowel(word)
 		noVowelBeforeQu := indexOfQu != -1 && indexOfVowel(word[:indexOfQu]) == -1
 		noVowelBeforeY := indexOfY > 0 && indexOfVowel(word[:indexOfY]) == -1
+		n := 0
 		if noVowelBeforeQu {
-			n := indexOfQu + 2
-			ret += word[n:] + word[:n] + "ay"
+			n = indexOfQu + 2
 		} else if noVowelBeforeY {
-			n := indexOfY
-			ret += word[n:] + word[:n] + "ay"
-		} else if beginWithVowel || beginWithXrYt {
-			ret += word + "ay"
-		} else if vowelIndex != -1 {
-			n := vowelIndex
-			ret += word[n:] + word[:n] + "ay"
-		} else {
-			ret += word
+			n = indexOfY
+		} else if !beginWithXrYt && vowelIndex != -1 {
+			n = vowelIndex
 		}
-		ret += " "
+		ret += word[n:] + word[:n] + "ay "
 	}
 	return strings.TrimSpace(ret)
 }
