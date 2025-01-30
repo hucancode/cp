82111a9971a1ae3eb4e1787fd1e09f4799d71a2d
USER <genji.nguyenhuu@ntq-solution.com.vn>
2025-01-30 11:45:08 +0900

    iter based solution

--- exercism/luhn/src/lib.rs
+++ exercism/luhn/src/lib.rs
@@ -1,23 +1,18 @@
 pub fn is_valid(code: &str) -> bool {
-    let mut sum = 0;
-    let mut count = 0;
-    for c in code.chars().rev() {
-        if c == ' ' {
-            continue;
-        }
-        if let Some(x) = c.to_digit(10) {
-            let mut val = x;
-            if count % 2 != 0 {
-                val *= 2;
-                if val > 9 {
-                    val -= 9;
-                }
-            }
-            sum += val;
-            count += 1;
-        } else {
-            return false;
-        }
+    let it = code.chars().filter(|&c| c != ' ').rev();
+    if it.clone().any(|c| !c.is_ascii_digit()) {
+        return false;
     }
-    sum % 10 == 0 && count > 1
+    let it = it.filter_map(|c| c.to_digit(10));
+    if it.clone().count() <= 1 {
+        return false;
+    }
+    let a: u32 = it.clone().step_by(2).sum();
+    let b: u32 = it
+        .skip(1)
+        .step_by(2)
+        .map(|n| n * 2)
+        .map(|x| if x > 9 { x - 9 } else { x })
+        .sum();
+    (a + b) % 10 == 0
 }
