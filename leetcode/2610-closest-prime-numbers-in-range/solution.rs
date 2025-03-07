5d8e686a518b4326adc5cb2e8ca057b3ae46a18a
Bang Nguyen Huu <hucancode@gmail.com>
2025-03-07 00:38:52 +0000

    Sync LeetCode submission Runtime - 118 ms (100.00%), Memory - 4 MB (100.00%)

---
+++ leetcode/2610-closest-prime-numbers-in-range/solution.rs
@@ -0,0 +1,36 @@
+impl Solution {
+    pub fn closest_primes(left: i32, right: i32) -> Vec<i32> {
+        let right = right as usize;
+        let left = left as usize;
+        let mut is_prime = vec![true; right + 1];
+        for i in 2..right {
+            if !is_prime[i] {
+                continue;
+            }
+            let mut k = 2;
+            loop {
+                let j = i*k;
+                if j > right {
+                    break;
+                }
+                is_prime[j] = false;
+                k += 1;
+            }
+        }
+        let primes: Vec<usize> = is_prime.into_iter()
+            .enumerate()
+            .skip(2)
+            .filter_map(|(i,x)| if x {Some(i)} else {None})
+            .collect();
+        let i = primes.partition_point(|&x| x < left);
+        let j = primes.partition_point(|&x| x <= right);
+        primes[i..j].windows(2)
+            .reduce(|a, b| {
+                if a[1] - a[0] > b[1] - b[0] {
+                    return b;
+                }
+                return a;
+            })
+            .map_or(vec![-1,-1], |a| vec![a[0] as i32, a[1] as i32])
+    }
+}
