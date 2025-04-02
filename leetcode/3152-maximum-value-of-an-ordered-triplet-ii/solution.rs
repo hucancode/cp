27a7044ec36ee734d74bd85c3dfc6698f727f970
Bang Nguyen Huu <hucancode@gmail.com>
2025-04-02 14:09:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 3.8 MB (53.33%)

---
+++ leetcode/3152-maximum-value-of-an-ordered-triplet-ii/solution.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn maximum_triplet_value(nums: Vec<i32>) -> i64 {
+        let n = nums.len();
+        let mut max_prefix = vec![0; n+1];
+        let mut max_suffix = vec![0; n+1];
+        let mut max_ij_prefix = vec![0; n+1];
+        for i in 1..=n {
+            max_prefix[i] = max_prefix[i-1].max(nums[i-1]);
+        }
+        for i in (0..n).rev() {
+            max_suffix[i] = max_suffix[i+1].max(nums[i]);
+        }
+        (1..n-1)
+            .filter(|&j| nums[j] < max_prefix[j])
+            .map(|j| (max_prefix[j] - nums[j]) as i64 * max_suffix[j+1] as i64)
+            .max()
+            .unwrap_or(0)
+    }
+}
