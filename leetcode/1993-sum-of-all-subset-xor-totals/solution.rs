93b8a05379df0663112e720821b664a79448d83c
Bang Nguyen Huu <hucancode@gmail.com>
2025-04-05 02:27:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 2.2 MB (64.07%)

--- leetcode/1993-sum-of-all-subset-xor-totals/solution.rs
+++ leetcode/1993-sum-of-all-subset-xor-totals/solution.rs
@@ -1,15 +1,6 @@
 impl Solution {
     pub fn subset_xor_sum(nums: Vec<i32>) -> i32 {
         let n = nums.len();
-        let m = 1<<n;
-        let mut ret = 0;
-        for mask in 1..m {
-            let mut x = 0;
-            for i in (0..n).filter(|i| (1<<i) & mask != 0) {
-                x ^= nums[i];
-            }
-            ret += x;
-        }
-        ret
+        nums.into_iter().fold(0, |acc, x| acc|x) << (n - 1)
     }
 }
