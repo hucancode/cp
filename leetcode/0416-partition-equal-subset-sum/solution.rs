80bf67124e2e401624a9c066156a93f8b00f9ac2
Bang Nguyen Huu <hucancode@gmail.com>
2025-04-07 00:51:55 +0000

    Sync LeetCode submission Runtime - 13 ms (67.11%), Memory - 2.3 MB (50.46%)

--- leetcode/0416-partition-equal-subset-sum/solution.rs
+++ leetcode/0416-partition-equal-subset-sum/solution.rs
@@ -1,8 +1,8 @@
 use std::cmp::min;
 impl Solution {
-    pub fn can_partition(nums: Vec<i32>) -> bool {
+    pub fn can_partition(mut nums: Vec<i32>) -> bool {
         let n: i32 = nums.iter().sum();
-        if n&1 != 0 {
+        if n%2 != 0 {
             return false;
         }
         let n = (n/2) as usize;
@@ -11,11 +11,9 @@ impl Solution {
         let mut cap = 0;
         for x in nums {
             let x = x as usize;
-            cap = min(cap + x, n);
-            let mut i = cap;
-            while i >= x {
+            cap = (cap+x).min(n);
+            for i in (x..=cap).rev() {
                 f[i] |= f[i-x];
-                i -= 1;
             }
         }
         return f[n];
